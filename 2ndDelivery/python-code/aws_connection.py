a7736b0e2f54c773aba0273a59b8fb541b2c9d05
Leonardo Razovic <leorazzo@gmail.com>
2020-05-26 10:54:44 +0200

    main code

--- 2ndDelivery/python-code/aws_connection.py
+++ 2ndDelivery/python-code/aws_connection.py
@@ -1,14 +1,14 @@
 # Import SDK packages
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 
-#import all needed packages
+# import all needed packages
 import time
 import json
 import datetime
 import random
 
 
-#Path for essential files
+# Path for essential files
 ROOT_CA = r"rootCa path"
 PRIVATE_KEY = r"private key path"
 CERTIFICATE = r"certificate path"
@@ -25,10 +25,11 @@ def customCallback(client, userdata, message):
     print(message.topic)
     print("--------------\n\n")
 
-def start_connection(topic, payload):
-    #This function set the connection with the MQTTClient of AWS
 
-    #Send the unique id of the station who is generating data
+def start_connection(topic, payload):
+    # This function set the connection with the MQTTClient of AWS
+
+    # Send the unique id of the station who is generating data
     myMQTTClient = AWSIoTMQTTClient("raspberry")
 
     myMQTTClient.configureEndpoint(ENDPOINT, 8883)
@@ -39,9 +40,7 @@ def start_connection(topic, payload):
     myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
     myMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
     myMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
-
-
-    #Start the connection, publish the data on the channel using the topic
+    # Start the connection, publish the data on the channel using the topic
     myMQTTClient.connect()
     myMQTTClient.publish(topic, payload, 0)
     myMQTTClient.subscribe(topic, 1, customCallback)
@@ -49,29 +48,14 @@ def start_connection(topic, payload):
     return myMQTTClient
 
 
+if __name__ == "__main__":
+    payload = {"raspberry space station": "I'm ON, mi ricevi?"}
+    print("Messagge sent: ", payload)
+    print("Waiting for messages on topic: ", TOPIC)
+    # Start the connection with AWS and wait for messages on topic
+    client = start_connection(TOPIC, json.dumps(payload))
 
-#------------------------------------------------------------------------------MAIN--------------------------------------------------------------
-
-
-payload = {"raspberry space station": "I'm ON, mi ricevi?"}
-
-print("Messagge sent: ", payload)
-
-print("Waiting for messages on topic: ", TOPIC )
-
-# Start the connection with AWS and wait for messages on topic
-client = start_connection(TOPIC, json.dumps(payload))
-
-# Wait infinitely for messages
-while(True):
-
-    # Save CPU resources 
-    time.sleep(100000) 
-
-
-
-
-
-
-
-
+    # Wait infinitely for messages
+    while True:
+        # Save CPU resources
+        time.sleep(100000)
