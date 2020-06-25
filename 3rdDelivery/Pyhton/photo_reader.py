b79646032cf790c74378927b3098fad4d8e1bd29
Leonardo Razovic <leorazzo@gmail.com>
2020-06-25 17:04:54 +0200

    better error handling

--- 3rdDelivery/Pyhton/photo_reader.py
+++ 3rdDelivery/Pyhton/photo_reader.py
@@ -1,39 +1,44 @@
 import os.path
 from os import path
 import cv2
-import requests # to get image from the web
-import shutil # to save it locally
+import requests  # to get image from the web
+import shutil  # to save it locally
 import os
-filename = "gc.jpg"
-img_url = "https://statues-iot.s3.eu-west-3.amazonaws.com/"+filename
-photopath = 'photo/'+filename
 
-if(path.exists(photopath)): 
-    img = cv2.imread(photopath)
-    cv2.imshow('image',img)
+filename = "gc"
+compression = "jpg"
+img_url = "https://statues-iot.s3.eu-west-3.amazonaws.com/{}.{}".format(
+    filename, compression
+)
+local_path = "./photo/" + filename
+
+if path.exists(local_path):
+    print("Image present locally")
+    img = cv2.imread(local_path)
+    cv2.imshow("image", img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 else:
-    r = requests.get(img_url, stream = True)
+    r = requests.get(img_url, stream=True)
     if r.status_code == 200:
-            print('Image sucessfully Downloaded: ',filename)
-            # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
-            r.raw.decode_content = True
-            with open('./photo/{}'.format(filename),'wb') as f:
-                f.write(r.content)
-            img = cv2.imread(photopath)
-            cv2.imshow('image',img)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
+        print("Image sucessfully downloaded: ", filename)
+        # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+        r.raw.decode_content = True
+        with open("./photo/{}".format(filename), "wb") as f:
+            f.write(r.content)
+        img = cv2.imread(local_path)
+        cv2.imshow("image", img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
-    else: 
-            print('Image not Downloaded: ',filename)
+    else:
+        print("Image {} not downloaded, error {}".format(filename, r.status_code))
 
 
-
-'''
+"""
 s3 = boto3.resource('s3', region_name='us-east-2')
 bucket = s3.Bucket('sentinel-s2-l1c')
 object = bucket.Object('image.jpg')
 tmp = tempfile.NamedTemporaryFile()
-'''
+"""
+
