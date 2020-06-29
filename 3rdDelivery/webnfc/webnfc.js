bc9acf4db6a67f1c58c31e676ce703ee62f51b99
Leonardo Razovic <leorazzo@gmail.com>
2020-06-29 10:04:28 +0200

    Web NFC site

---
+++ 3rdDelivery/webnfc/webnfc.js
@@ -0,0 +1,20 @@
+async function write(event) {
+  let name = event.srcElement.name;
+  try {
+    const writer = new NDEFWriter();
+    textField.style.color = "green";
+    textField.innerHTML = "Move your smartphone closer to the receiver";
+    await writer.write(name).then(() => {
+      textField.innerHTML = "Message sent successfully";
+    });
+  } catch (error) {
+    textField.style.color = "red";
+    textField.innerHTML = `Error: ${error}`;
+  }
+}
+
+const buttons = document.querySelectorAll("body > div > button");
+const textField = document.getElementById("message");
+buttons.forEach((button) => {
+  button.addEventListener("click", write);
+});
