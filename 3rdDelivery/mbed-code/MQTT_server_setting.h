a8f25dd37098e2953385e38867a04cfa0c480be9
Leonardo Razovic <leorazzo@gmail.com>
2020-06-30 11:26:24 +0200

    Update MQTT_server_setting.h

--- 3rdDelivery/mbed-code/MQTT_server_setting.h
+++ 3rdDelivery/mbed-code/MQTT_server_setting.h
@@ -4,9 +4,7 @@
 
 const char MQTT_SERVER_HOST_NAME[] =
     "a3bz5hd2mygo9b-ats.iot.eu-west-3.amazonaws.com";
-const char MQTT_CLIENT_ID[] = "smart_device";
-const char MQTT_USERNAME[] = "";
-const char MQTT_PASSWORD[] = "";
+const char MQTT_CLIENT_ID[] = "mbedos_board_1";
 const char MQTT_TOPIC_PUB[] = "/board";
 const char MQTT_TOPIC_SUB[] = "/board";
 const int MQTT_SERVER_PORT = 8883;
